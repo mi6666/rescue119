3e1354e5516818d18ba9ad4b6baa03c7b9f1a4ef
KasuyaShota <22rd055@ms.dendai.ac.jp>
2025-10-06 23:57:58 +0900

    fix scene module

--- Assets/Scripts/Module/SceneReference/Runtime/SceneGroup.cs
+++ Assets/Scripts/Module/SceneReference/Runtime/SceneGroup.cs
@@ -1,15 +1,44 @@
-using System;
 using UnityEngine;
 
 namespace Module.SceneReference.Runtime
 {
-    [Serializable]
-    public class SceneGroup
+    [CreateAssetMenu(fileName = "SceneGroup", menuName = "SceneGroup")]
+    public class SceneGroup : ScriptableObject
     {
-        [SerializeField] private string primaryScene;
-        [SerializeField] private string[] subScenes;
+        [SerializeField] private SceneField primaryScene;
+        [SerializeField] private SceneField[] subScenes;
+
+        private string[] _subScenePathsCache;
+        private bool _isCacheDirty = true;
 
         public string PrimaryScene => primaryScene;
-        public string[] SubScenes => subScenes;
+        public string[] SubScenes
+        {
+            get
+            {
+                if (_isCacheDirty || _subScenePathsCache == null)
+                {
+                    if (subScenes == null)
+                    {
+                        _subScenePathsCache = System.Array.Empty<string>();
+                    }
+                    else
+                    {
+                        _subScenePathsCache = new string[subScenes.Length];
+                        for (var i = 0; i < subScenes.Length; i++)
+                        {
+                            _subScenePathsCache[i] = subScenes[i];
+                        }
+                    }
+                    _isCacheDirty = false;
+                }
+                return _subScenePathsCache;
+            }
+        }
+
+        private void OnValidate()
+        {
+            _isCacheDirty = true;
+        }
     }
 }
